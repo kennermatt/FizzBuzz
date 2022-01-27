608bbaed2528c2bbf86d5490a0ba3123619cc168
kennermatt <kennermatt@gmail.com>
2022-01-27 09:23:26 -0700

    added comments

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -1,3 +1,25 @@
+/**
+ * Created by: Matthew Kenner 1/27/2022
+ * CS251 Lab01 - FizzBuzz
+ *
+ * The following program writes Fizz and Buzz for each multiple of
+ * whatever the user inputs.
+ * To use this program follow this guideline in terminal:
+ *     javac FizzBuzz.java
+ *     java FizzBuzz 3 5 100
+ *
+ * class FizzBuzz
+ *  This method requires the user to input 3 numbers in terminal:
+ *  1st number is the first multiple fizz
+ *  2nd number is the second multiple buzz
+ *  3rd number is the upper limit of the program
+ *
+ *  The program then checks to see if the current number is a multiple
+ *  of the 1st or 2nd number and writes Fizz or Buzz respectively.
+ *
+ *  The program terminates when the upper limit has been reached.
+ *  return void
+ */
 public class FizzBuzz {
     public static void main(String[] args){
         if(args.length != 3){
@@ -6,17 +28,17 @@ public class FizzBuzz {
             int fizz = Integer.parseInt(args[0]);
             int buzz = Integer.parseInt(args[1]);
             int upperLimit = Integer.parseInt(args[2]);
-            for (int i = 1; i <= upperLimit; i++){
-                if (i % fizz == 0){
-                    if (i % buzz == 0){
+            for (int j = 1; j <= upperLimit; j++){
+                if (j % fizz == 0){
+                    if (j % buzz == 0){
                         System.out.println("FizzBuzz");
                     } else {
                         System.out.println("Fizz");
                     }
-                } else if (i % buzz == 0) {
+                } else if (j % buzz == 0) {
                     System.out.println("Buzz");
                 } else {
-                    System.out.println(i);
+                    System.out.println(j);
                 }
             }
         }
